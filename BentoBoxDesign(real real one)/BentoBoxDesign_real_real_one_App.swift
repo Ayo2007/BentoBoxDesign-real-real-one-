6f2740a625e6af600b1c01e5f67cc739beb844a5
Ayo <aogunkinle@lcs.on.ca>
2023-10-27 11:47:49 -0400

    Initial Commit

---
+++ BentoBoxDesign(real real one)/BentoBoxDesign_real_real_one_App.swift
@@ -0,0 +1,17 @@
+//
+//  BentoBoxDesign_real_real_one_App.swift
+//  BentoBoxDesign(real real one)
+//
+//  Created by Ayodeji Ogunkinle on 2023-10-27.
+//
+
+import SwiftUI
+
+@main
+struct BentoBoxDesign_real_real_one_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
